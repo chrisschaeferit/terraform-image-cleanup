290d8ba33a0d207b1581a885bb70440c102152da
Schaefer, Chris <cschaefer@entertainment.com>
2018-08-31 09:23:01 -0400

    email

--- sns.tf
+++ sns.tf
@@ -3,6 +3,6 @@ resource "aws_sns_topic" "user_updates" {
 
 
   provisioner "local-exec" {
-    command = "aws sns subscribe --topic-arn ${self.arn} --protocol email --notification-endpoint cschaefer@entertainment.com"
+    command = "aws sns subscribe --topic-arn ${self.arn} --protocol email --notification-endpoint test@test.com"
   }
 }
