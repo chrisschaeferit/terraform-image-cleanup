949ea4035804c3c4a5541071b38281e2d63c8f08
Schaefer, Chris <cschaefer@entertainment.com>
2018-08-31 09:23:01 -0400

    email

--- sns.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_sns_topic" "user_updates" {
-  name = "${var.aws_region}_ec2_amicleanup_notify"
-
-
-  provisioner "local-exec" {
-    command = "aws sns subscribe --topic-arn ${self.arn} --protocol email --notification-endpoint cschaefer@entertainment.com"
-  }
-}
